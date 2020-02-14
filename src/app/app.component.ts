f0579458c29eeb39237d846a58818945de070921
Arpita Tugave <arpita.subash.tugave@gmail.com>
2020-02-13 18:33:00 -0600

    Add logging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -69,8 +69,10 @@ export class AppComponent implements OnInit {
 
   nextStep = () => {
     console.log('Success!');
+    console.log(this.token);
 
     if (this.step === 4) {
+      console.log('Here')
       this.sdk.processId({
         token: this.token.token,
         queueName: ''
